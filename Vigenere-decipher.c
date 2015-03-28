adef1dcf88d105c3ed598ba0e7cef839fbf09c51
arnulfo perez <arnulfo@debian.workgroup.com>
2015-03-27 22:05:13 -0500

    reading input
    first stage

---
+++ Vigenere-decipher.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h> 
+
+unsigned char  hex_to_int(char c){
+        unsigned char  first = c / 16 - 3;
+        unsigned char  second = c % 16;
+        unsigned char  result = first*10 + second;
+        if(result > 9) result--;
+        return result;
+}
+
+unsigned char hex_to_ascii(char c, char d){
+        unsigned char  high = hex_to_int(c) * 16;
+        unsigned char  low = hex_to_int(d);
+        return high+low;
+}
+
+int main(){
+	const char* PLAINTEXT = "dtext.txt";
+	const char* CIPHERTEXT = "ctext.txt";
+	unsigned char ch;
+	unsigned char *ptr;
+        FILE *fpIn, *fpOut;
+        int i,length,fd;
+        unsigned char buf = 0;
+	 
+	fd = open(CIPHERTEXT, O_RDONLY );
+	length = lseek(fd, 0, SEEK_END)+1;
+	close(fd);
+	ptr=(unsigned char*)malloc(length*sizeof(unsigned char));
+	 
+	fpIn = fopen(CIPHERTEXT, "r");
+	fpOut = fopen(PLAINTEXT, "w");
+	i = 0;
+
+  while (fscanf(fpIn, "%c", &ch) != EOF) {
+    /* avoid encrypting newline characters */  
+    /* In a "real-world" implementation of the Vigenere cipher, 
+       every ASCII character in the plaintext would be encrypted.
+       However, I want to avoid encrypting newlines here because 
+       it makes recovering the plaintext slightly more difficult... */
+    /* ...and my goal is not to create "production-quality" code =) */
+    if (ch=='\n') {
+	    *(ptr + i) = ch;
+	    i++;
+    }
+    else {
+	    buf = ch;
+	    fscanf(fpIn, "%c", &ch) ;
+	    *(ptr + i) = hex_to_ascii(buf, ch);
+	    i++;
+      }
+    }
+     return(0);
+}
